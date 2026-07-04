173878c9b3b84b0081b079df8bc2d37192f798c2
patidarmithil <mithilpatidar80@gmail.com>
2026-07-04 15:43:56 +0530

    documentation added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -270,7 +270,7 @@ export default function Navbar() {
           
           {(location.pathname === '/intro' || location.pathname === '/about' || (location.pathname === '/' && !isLoggedIn)) ? (
             <>
-              <a href="https://drive.google.com/file/d/1Z4cJntacHK7yXh1dub5aEJNe38mWkZD5/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn--ghost btn--sm" style={{ marginRight: '8px' }}>
+              <a href="https://drive.google.com/file/d/1PcqTQEhuijVDgiO9vu-e9unlm5KCt1qx/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn--ghost btn--sm" style={{ marginRight: '8px' }}>
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ marginRight: '6px' }}>
                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
                   <polyline points="14 2 14 8 20 8"></polyline>
@@ -633,7 +633,7 @@ export default function Navbar() {
                       </svg> About
                     </Link>
                     <a
-                      href="https://drive.google.com/file/d/1Z4cJntacHK7yXh1dub5aEJNe38mWkZD5/view?usp=sharing"
+                      href="https://drive.google.com/file/d/1PcqTQEhuijVDgiO9vu-e9unlm5KCt1qx/view?usp=sharing"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="navbar__dropdown-item"
